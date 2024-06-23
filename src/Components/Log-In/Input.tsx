7bf2d0b255bae193a3463e2011857df4247379b3
Miguel <miguelsribeiro7@gmail.com>
2024-06-23 19:06:43 +0100

    login base

--- src/Components/Log-In/Input.tsx
+++ src/Components/Log-In/Input.tsx
@@ -1,13 +1,32 @@
 const Input = () => {
   return (
-    <div className="flex flex-col gap-y-5">
-      <div className="flex flex-col gap-y-3">
-        <label htmlFor="username">Username:</label>
-        <input type="text" id="username" />
-        <label htmlFor="password">Password:</label>
-        <input type="password" id="password" />
-      </div>
-      <input type="submit" className="border-2 cursor-pointer" />
+    <div className="mb-5 flex flex-col gap-y-5">
+      <form>
+        <div className="mb-10 flex flex-col gap-y-3">
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            required
+            id="username"
+            className="border-2 border-orange-300 p-2"
+          />
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            required
+            id="password"
+            className="border-2 border-orange-300 p-2"
+          />
+        </div>
+        <input
+          type="submit"
+          value="Log In"
+          className="w-full cursor-pointer rounded-lg bg-orange-400 p-2 text-white transition duration-200 ease-in-out hover:bg-orange-500"
+        />
+      </form>
+      <button className="cursor-pointer rounded-lg border-2 border-orange-300 p-2 transition duration-200 ease-in-out hover:bg-orange-500 hover:text-white">
+        Register
+      </button>
     </div>
   );
 };
--- src/Components/Log-In/LogIn.tsx
+++ src/Components/Log-In/LogIn.tsx
@@ -1,9 +1,11 @@
-import Input from './Input';
+import Input from "./Input";
 
 const LogIn = () => {
   return (
-    <div className="border-2 p-10 rounded-lg w-1/4 h-2/3 ">
-      <h1 className="mb-10">.admin</h1>
+    <div className="w-4/5 rounded-md border-orange-500 bg-white p-6 shadow-md outline-black">
+      <h1 className="mb-10 mt-5 self-end text-3xl font-bold">
+        <span className="mr-1 text-orange-500">.</span>admin
+      </h1>
       <Input></Input>
     </div>
   );
