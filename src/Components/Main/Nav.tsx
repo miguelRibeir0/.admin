e98cfbd9295c7dacefc0cad4e417b85c6f6a22b1
Miguel <miguelsribeiro7@gmail.com>
2024-07-13 00:17:41 +0100

    settings base

--- src/Components/Main/Nav.tsx
+++ src/Components/Main/Nav.tsx
@@ -3,6 +3,17 @@ import { Spiral as Hamburger } from "hamburger-react";
 import SideBar from "./SideBar";
 import Logo from "../Logo";
 import settings from "../../assets/settings.svg";
+import { Button } from "@/Components/ui/button";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/Components/ui/sheet";
 
 const Nav = () => {
   const [isOpen, setOpen] = useState(false);
@@ -16,7 +27,60 @@ const Nav = () => {
           </div>
           <Logo color={""} />
           <div className="w-12">
-            <img src={settings} className="w-6 cursor-pointer text-xs lg:w-8" />
+            <Sheet>
+              <SheetTrigger asChild>
+                <img
+                  src={settings}
+                  className="w-6 cursor-pointer text-xs lg:w-8"
+                />
+              </SheetTrigger>
+              <SheetContent className="border-l-2 border-orange-500">
+                <SheetHeader>
+                  <SheetTitle>Edit profile</SheetTitle>
+                  <SheetDescription>
+                    Make changes to your profile here. Click save when you're
+                    done.
+                  </SheetDescription>
+                </SheetHeader>
+                <div className="grid gap-4 py-4">
+                  <div className="flex flex-col gap-4">
+                    <label htmlFor="user">User:</label>
+                    <input
+                      type="text"
+                      id="user"
+                      className="h-10 border-2 border-orange-500 bg-orange-50 pl-2"
+                      disabled
+                    />
+                  </div>
+                  <div className="flex flex-col gap-4">
+                    <label htmlFor="password">Password:</label>
+                    <input
+                      type="password"
+                      id="password"
+                      className="h-10 border-2 border-orange-500 bg-orange-50 pl-2"
+                      disabled
+                    />
+                  </div>
+                  <div className="flex flex-col gap-4">
+                    <label htmlFor="email">Email:</label>
+                    <input
+                      type="text"
+                      id="email"
+                      className="h-10 border-2 border-orange-500 bg-orange-50 pl-2"
+                      placeholder="placeholderemail123@gmail.com"
+                      disabled
+                    />
+                  </div>
+                </div>
+                <SheetFooter className="pt-5">
+                  <SheetClose asChild>
+                    <Button type="submit" className="bg-orange-500">
+                      Save changes
+                    </Button>
+                  </SheetClose>
+                </SheetFooter>
+              </SheetContent>
+            </Sheet>
           </div>
         </div>
       </nav>
